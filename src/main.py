547008359550fc483d4ea722168fcabdb5d4fbfc
SethK99 <sgkloster@gmail.com>
2025-01-03 18:58:11 -0800

    added recursive function to copy contents from source to destination directory

--- src/main.py
+++ src/main.py
@@ -1,11 +1,34 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_directory_recursive(source,destination):
+
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination_path)
+            print(f"Copied file: {source_path} -> {destination_path}")
+
+        elif os.path.isdir(source_path):
+            os.mkdir(destination_path)
+            print(f"Created directory: {destination_path}")
+            copy_directory_recursive(source_path,destination_path)
+
 
 def main():
-    # Create a TextNode object with dummy values
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    
-    # Print the object
-    print(text_node)
+    source_dir = "static"
+    destination_dir = "public"
+
+    copy_directory_recursive(source_dir, destination_dir)
+    print("All files and directories copied successfully")
 
 if __name__ == "__main__":
     main()
